3e38b3855d2ae8da1160833f8df7bc689c3b0959
RomanSeps <96355457+RomanSeps@users.noreply.github.com>
2025-01-27 17:11:26 +0100

    Update select.sql

--- select.sql
+++ select.sql
@@ -1,4 +1,14 @@
 USE `products` ;
 
-SELECT product.name FROM product LEFT JOIN vendor
-ON vendor.id = product.vendor_id;
+SELECT 
+    v.name AS vendor_name, 
+    COUNT(p.id) AS product_count
+FROM 
+    vendor v
+LEFT JOIN 
+    product p ON v.id = p.vendor_id
+GROUP BY 
+    v.id, v.name
+ORDER BY 
+    product_count DESC
+LIMIT 10;
